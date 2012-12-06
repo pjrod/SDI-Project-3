54da9aa8665565281652819b59e4b8999b8592cf
Pedro Rodriguez <pjrod@fullsail.edu>
2012-12-06 01:49:14 -0500

    Updated js file with string function

--- rodriguez_pedro_project3.js
+++ rodriguez_pedro_project3.js
@@ -11,7 +11,10 @@ var warpDrive          = "Warp Drive",
 	pirateSpaceShips   = ["Noob", "Mortar", "Interceptor", "Weaver", "Kamari"],
 	warpSpeed          = [ 1, 2, 3, 4, 5,],
 	planetarySystems   = 10,
-	developedWarpDrive = true;
+	developedWarpDrive = true,
+    planet1	           = "reptile",
+    planet2            = "Omega",
+    reptilian 		   = true;
     
 //Procedure
 var warpDriveIsAGo = developedWarpDrive; {
@@ -23,10 +26,52 @@ var warpDriveIsAGo = developedWarpDrive; {
 	}
 };	
 
-//String Function
+//Main Code
+console.log("Since the development of " + warpDrive + " humans have been exploring the galaxy.");
+console.log("They have discovered many planetary systems and species.");
+console.log("The first two species discovered where Aquatic and Reptilian.");
 
+//String Function
+var planets = function(planet1,planet2){
+	if (planet1 === "reptile"){
+		return ("Reptilians came from Nebulon.");
+	} else {
+		var otherPlanet = "Omega" ;
+		return "Reptilians did not come from " + otherPlanet;
+	}
+};
+
+//Main Code
+
+var outcome = planets(planet1);
+console.log(outcome);
+console.log("Aquatics came from " + planet2 + ".");
+
+//Number Function
+
+
+
+
+
+
+/*var planets = function(planet1,planet2){
+	var species = [planet1, planet2];
+
+	return species;
 	
-    
+};
+if (reptilian === true){
+		var whereFrom1 = planets(planet1);
+		console.log("Reptilians came from " + whereFrom1);
+	} else {
+		var whereFrom2 = planets(planet2);
+		console.log("Reptilians did not come from " + whereFrom2);
+	};
+
+//Main Code
+console.log("The Aquatics came from " + planet2) 
+*/
+
 
 
 //for reference
